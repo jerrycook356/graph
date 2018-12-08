35c2a81cdccb355b1dcfbc99315aa1fefe097f38
jerrycook356 <Jerrycook356@gmail.com>
2018-12-08 11:55:09 -0500

    a

---
+++ GraphApplication/Graph.cpp
@@ -0,0 +1,58 @@
+#include "Graph.h"
+
+
+template<class LabelType>
+Graph<LabelType>::Graph(int numberOfVertices)
+{
+	adjMatrix = new int[numberOfVertices, numberOfVertices];
+	vertices = numberOfVertices;
+	
+}
+
+template<class LabelType>
+int Graph<LabelType>::getNumVertices()const
+{
+	return vertices;
+	
+}
+
+template<class LabelType>
+int Graph<LabelType>::getNumEdges() const
+{
+	return 0;
+}
+
+template<class LabelType>
+bool Graph<LabelType>::add(LabelType start, LabelType end, int edgeWeight)
+{
+	int weight = edgeWeight;
+	adjMatrix[start, end] = 1;
+	
+
+	return true;
+}
+
+template<class LabelType>
+bool Graph<LabelType>::remove(LabelType start, LabelType end)
+{
+	adjMatrix[start, end] = 0;
+	return true;
+}
+
+template<class LabelType>
+int Graph<LabelType>::getEdgeWeight(LabelType start, LabelType end) const
+{
+	return 0;
+}
+
+template<class LabelType>
+void Graph<LabelType>::breadthFirstTraversal(LabelType start,  void visit(LabelType&))
+{
+
+}
+
+template<class LabelType>
+void Graph<LabelType>::depthFirstTraversal(LabelType start, void visit(LabelType&))
+{
+
+}
---
+++ GraphApplication/Graph.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "GraphInterface.h"
+#include <vector>
+
+using namespace std;
+
+template<class LabelType>
+class Graph :public GraphInterface<LabelType>
+{
+
+private:
+
+	int* adjMatrix;
+	int vertices;
+
+public:
+
+	Graph(int numberOfVertices);
+
+	int getNumVertices()const;
+	int getNumEdges()const;
+	bool add(LabelType start, LabelType end, int edgeWeight);
+    bool remove(LabelType start, LabelType end);
+	int getEdgeWeight(LabelType start, LabelType end)const;
+	void breadthFirstTraversal(LabelType start, void visit(LabelType&));
+	void depthFirstTraversal(LabelType start, void visit(LabelType&));
+
+
+
+};
+#include "Graph.cpp"
+
